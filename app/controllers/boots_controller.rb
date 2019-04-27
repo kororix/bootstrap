675eda3c3d27333e9c577d3dad2190651cca1fbb
Ayaka Kawai <consultation.pharmacist6@gmail.com>
2019-04-28 00:28:36 +0900

    投稿画面作成

--- app/controllers/boots_controller.rb
+++ app/controllers/boots_controller.rb
@@ -1,4 +1,19 @@
 class BootsController < ApplicationController
   def index
   end
+
+  def new
+    @boot = Boot.new
+  end
+
+  def create
+    Boot.create(boot_params)
+    redirect_to new_boot_path
+  end
+
+  private
+
+  def boot_params
+  params.require(:boot).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get '/boots', to: 'boots#index'
+  resources :boots
 end
